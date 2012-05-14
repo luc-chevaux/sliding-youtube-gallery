a9b609e055073415db55a41de16b0f71fc4d299c
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-14 20:55:34 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@544248 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- engine/SygConstant.php
+++ engine/SygConstant.php
@@ -1,16 +1,16 @@
 <?php
 class SygConstant {
 	const SYG_VERSION = '1.2.0';
-	
+
 	// define administration menu constant
 	const BE_WELCOME_MESSAGE = "Il lorem ipsum è un insieme di parole utilizzato da grafici, designer, programmatori e tipografi come testo riempitivo in bozzetti e prove grafiche[1]. È un testo privo di senso, composto da parole in lingua latina (spesso storpiate), riprese pseudocasualmente da uno scritto di Cicerone del 45 a.C.";
 	const BE_SUPPORT_PAGE = "";
 	const BE_DONATION_CODE = "";
-	
+
 	// methods
 	const SYG_METHOD_GALLERY = 'gallery';
 	const SYG_METHOD_PAGE = 'page';
-	
+
 	// paths 
 	const WP_PLUGIN_PATH = '/wp-content/plugins/sliding-youtube-gallery/';
 	const WP_CSS_PATH = '/wp-content/plugins/sliding-youtube-gallery/css/';
