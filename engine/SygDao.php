2c9e3457b344d997a1d86740d8bd631f540e89c0
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-14 20:56:06 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@544249 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- engine/SygDao.php
+++ engine/SygDao.php
@@ -4,7 +4,10 @@ global $wpdb;
 
 class SygDao {
 	private $wpDatabaseLink;
-		
+	
+	// query
+	private $sqlGetAllGalleries = 'SELECT id, syg_youtube_username, syg_youtube_videoformat, syg_youtube_maxvideocount, syg_thumbnail_height, syg_thumbnail_width, syg_thumbnail_bordersize, syg_thumbnail_bordercolor, syg_thumbnail_borderradius, syg_thumbnail_distance, syg_thumbnail_overlaysize, syg_thumbnail_image, syg_thumbnail_buttonopacity, syg_description_width, syg_description_fontsize, syg_description_fontcolor, syg_description_show, syg_description_showduration, syg_description_showtags, syg_description_showratings, syg_description_showcategories, syg_box_width, syg_box_background, syg_box_radius, syg_box_padding FROM wp_syg';
+	
 	// default constructor
 	public function __construct() {
 		$this->wpDatabaseLink = $wpdb;
@@ -26,8 +29,8 @@ class SygDao {
 	}
 	
 	// get all syg gallery from database
-	function getAllSyg() {
-		
+	function getAllSyg($output_type = 'OBJECT') {
+		return $wpdb->get_results($this->sqlGetAllGalleries, $output_type);
 	}
 
 	// get syg gallery by id from database
