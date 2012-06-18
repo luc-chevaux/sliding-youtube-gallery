18701dc264c5d3ac1dd8e794c071e4bf254183aa
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-18 20:43:31 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@560154 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/gallery.php
+++ views/gallery.php
@@ -17,9 +17,9 @@ $overlayButtonSrc = (!empty($thumbImage)) ? $this->data['imgPath'] . '/button/pl
 <!-- Javascript Inclusion -->
 
 <!-- Gallery -->
-<div id="syg_video_gallery-<?php echo $gallery->getId();?>">
+<div id="syg_video_gallery-<?php echo $gallery->getId();?>" class="syg_video_gallery_loading-<?php echo $gallery->getId();?>">
 	<div class="sc_menu-<?php echo $gallery->getId();?>">
-		<ul class="sc_menu-<?php echo $gallery->getId();?>">
+		<ul class="sc_menu-<?php echo $gallery->getId();?>" style="display: none;">
 			<?php 
 				foreach ($feed as $element) {
 					$videoThumbnails = $element->getVideoThumbnails();	
@@ -49,3 +49,41 @@ $overlayButtonSrc = (!empty($thumbImage)) ? $this->data['imgPath'] . '/button/pl
 		</ul>
 	</div>
 </div>
+
+<script>
+jQuery(window).load(function() {
+
+	jQuery('#syg_video_gallery-<?php echo $gallery->getId(); ?>')
+		.removeClass('syg_video_gallery_loading-<?php echo $gallery->getId(); ?>')
+		.addClass('syg_video_gallery-<?php echo $gallery->getId(); ?>');
+	
+	
+	/* remove display none */
+	jQuery('.sc_menu-<?php echo $gallery->getId();?>').removeAttr('style');
+
+	jQuery(function($){
+		//Get our elements for faster access and set overlay width
+		var div = $('div.sc_menu-<?php echo $gallery->getId(); ?>'),
+			ul = $('ul.sc_menu-<?php echo $gallery->getId(); ?>'),
+			ulPadding = 15;
+		
+		//Get menu width
+		var divWidth = <?php echo $gallery->getBoxWidth(); ?>;
+		
+		//Remove scrollbars	
+		div.css({overflow: 'hidden'});
+		
+		//Find last image container
+		var lastLi = ul.find('li:last-child');
+		
+		//When user move mouse over menu
+		div.mousemove(function(e){
+			//As images are loaded ul width increases,
+			//so we recalculate it each time
+			var ulWidth = lastLi[0].offsetLeft + lastLi.outerWidth() + ulPadding;	
+			var left = (e.pageX - div.offset().left) * (ulWidth-divWidth) / divWidth;
+			div.scrollLeft(left);
+		});
+	});
+});
+</script>
