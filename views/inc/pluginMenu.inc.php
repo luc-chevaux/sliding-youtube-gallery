3927cd2c25dbcdca7a2fa5b60776ec5dd2dd6da6
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-08 23:43:33 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@569116 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/inc/pluginMenu.inc.php
+++ views/inc/pluginMenu.inc.php
@@ -1,14 +1,14 @@
 <p>
-<a href="options-general.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_GALLERIES; ?>" class="button-primary">
+<a href="admin.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_GALLERIES; ?>" class="button-primary">
 	<?php echo SygConstant::BE_MENU_MANAGE_GALLERIES; ?>
 </a>
-<a href="options-general.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_STYLES; ?>" class="button-primary">
+<a href="admin.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_STYLES; ?>" class="button-primary">
 	<?php echo SygConstant::BE_MENU_MANAGE_STYLES; ?>
 </a>
-<a href="options-general.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_SETTINGS; ?>" class="button-primary">
+<a href="admin.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_SETTINGS; ?>" class="button-primary">
 	<?php echo SygConstant::BE_MENU_MANAGE_SETTINGS; ?>
 </a> 
-<a href="options-general.php?page=<?php echo SygConstant::BE_ACTION_CONTACTS; ?>" class="button-primary">
+<a href="admin.php?page=<?php echo SygConstant::BE_ACTION_CONTACTS; ?>" class="button-primary">
 	<?php echo SygConstant::BE_MENU_CONTACTS_AND_SUPPORT; ?>
 </a>
 </p>
