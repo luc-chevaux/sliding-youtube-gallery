f4252f856684de7a56039e2ad6b395a75c9343bf
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-08 11:25:36 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@568946 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/inc/contextMenu.inc.php
+++ views/inc/contextMenu.inc.php
@@ -2,18 +2,18 @@
 <p>
 
 <?php if ($_GET['page'] == 'syg-manage-styles') { ?>
-	<a href="options-general.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_STYLES; ?>&action=add" class="button-primary">
+	<a href="admin.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_STYLES; ?>&action=add" class="button-primary">
 		<?php echo SygConstant::BE_MENU_ADD_NEW_STYLE; ?>
 	</a>
 <?php } ?>
 
 <?php if ($_GET['page'] == 'syg-manage-galleries') { ?>
-	<a href="options-general.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_GALLERIES; ?>&action=add" class="button-primary">
+	<a href="admin.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_GALLERIES; ?>&action=add" class="button-primary">
 		<?php echo SygConstant::BE_MENU_ADD_NEW_GALLERY; ?>
 	</a>
 <?php } ?>
 
-	<a href="options-general.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_GALLERIES; ?>" class="button-primary">
+	<a href="admin.php?page=<?php echo SygConstant::BE_ACTION_MANAGE_GALLERIES; ?>" class="button-primary">
 		<?php echo SygConstant::BE_MENU_JUMP_TO_HOME; ?>
 	</a>
 	
