4f4916749e73d1e2dd98854b31d0f7780406ea20
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-07 17:35:01 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@582771 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- engine/data/data.php
+++ engine/data/data.php
@@ -1,6 +1,6 @@
 <?php 
 
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 
 header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . 'GMT' );
 header('Cache-Control: no-cache, must-revalidate' );
@@ -30,51 +30,83 @@ require_once ('../SygUtil.php');
 $plugin = SygPlugin::getInstance();
 
 if ($plugin->verifyAuthToken($_SESSION['request_token'])) {
-	switch ($_GET['table']) {
-		case 'galleries': 
-			if($_GET['page_number']) {
-				$page_number = $_GET['page_number'];
-				$current_page = $page_number;
-				$page_number -= 1;
-					
-				$per_page = SygConstant::SYG_CONFIG_NUMBER_OF_RECORDS_DISPLAYED; // Per page records
-					
-				$start = $page_number * $per_page;
-					
-				$dao = new SygDao();
-				$galleries = $dao->getAllSygGalleries('OBJECT', $start, $per_page);
-
-				$gallery_to_json = array();
-			
-				foreach ($galleries as $gallery) {
-					array_push($gallery_to_json, $gallery->getJsonData());
+	if ($_GET['table']) {
+		switch ($_GET['table']) {
+			case 'galleries': 
+				if($_GET['page_number']) {
+					$page_number = $_GET['page_number'];
+					$current_page = $page_number;
+					$page_number -= 1;
+						
+					$per_page = SygConstant::SYG_CONFIG_NUMBER_OF_RECORDS_DISPLAYED; // Per page records
+						
+					$start = $page_number * $per_page;
+						
+					$dao = new SygDao();
+					$galleries = $dao->getAllSygGalleries('OBJECT', $start, $per_page);
+	
+					$gallery_to_json = array();
+				
+					foreach ($galleries as $gallery) {
+						array_push($gallery_to_json, $gallery->getJsonData());
+					}
+				
+					echo json_encode ($gallery_to_json);
 				}
-			
-				echo json_encode ($gallery_to_json);
-			}
-			break;
-		case 'styles':
-			if($_GET['page_number']) {
-				$page_number = $_GET['page_number'];
-				$current_page = $page_number;
-				$page_number -= 1;
-					
-				$per_page = SygConstant::SYG_CONFIG_NUMBER_OF_RECORDS_DISPLAYED; // Per page records
-
-				$start = $page_number * $per_page;
-					
-				$dao = new SygDao();
-				$styles = $dao->getAllSygStyles('OBJECT', $start, $per_page);
-					
-				$style_to_json = array();
-					
-				foreach ($styles as $style) {
-					array_push($style_to_json, $style->getJsonData());
+				break;
+			case 'styles':
+				if($_GET['page_number']) {
+					$page_number = $_GET['page_number'];
+					$current_page = $page_number;
+					$page_number -= 1;
+						
+					$per_page = SygConstant::SYG_CONFIG_NUMBER_OF_RECORDS_DISPLAYED; // Per page records
+	
+					$start = $page_number * $per_page;
+						
+					$dao = new SygDao();
+					$styles = $dao->getAllSygStyles('OBJECT', $start, $per_page);
+						
+					$style_to_json = array();
+						
+					foreach ($styles as $style) {
+						array_push($style_to_json, $style->getJsonData());
+					}
+						
+					echo json_encode ($style_to_json);
 				}
+				break;
+		}
+	} elseif ($_GET['query']) {
+		switch ($_GET['query']) {
+			case 'videos':
+				if($_GET['page_number']) {
+					$page_number = $_GET['page_number'];
+					$current_page = $page_number;
+					$page_number -= 1;
 					
-				echo json_encode ($style_to_json);
-			}
-			break;
+					$options = $plugin->getOptions();
+					
+					$per_page = $options['syg_option_pagenumrec']; // Per page records
+					
+					$start = $page_number * $per_page;
+					
+					$dao = new SygDao();
+					$gallery = $dao->getSygGalleryById($_GET['id']);
+					
+					$videos = $plugin->getVideoFeed($gallery);
+					$videos_to_json = array();
+					
+					foreach ($videos as $video) {
+						array_push($video, $gallery->getJsonData());
+					}
+					
+					echo json_encode ($videos_to_json);
+				}
+				break;
+			default: 
+				NULL; break;
+		}
 	}
 }
 ?>
