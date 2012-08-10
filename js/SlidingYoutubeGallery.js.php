dcc8599b31d4105a142ec53e9ba5c569df0dec6b
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-10 23:38:44 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@584092 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/SlidingYoutubeGallery.js.php
+++ js/SlidingYoutubeGallery.js.php
@@ -19,6 +19,10 @@ require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 require_once( ABSPATH . 'wp-content/plugins/sliding-youtube-gallery/engine/SygPlugin.php');
 
 $syg = SygPlugin::getInstance();
+
+/*$syg = new SygPlugin();
+$syg->getPluginRoot()*/
+
 $option = $syg->getGallerySettings($_GET['id']);
 extract ($option);
 
@@ -35,17 +39,171 @@ $width += 20;
 $height += 20;
 
 ?>
+    
+jQuery.noConflict();
+
+(function($){
+	/**
+	 * function applied to selector
+	 *
+	 * $.fn.calculateNewWidth = function() {
+	 * return true;
+	 * };
+	 */
+	
+	$.extend({
+		
+		/**
+		 * function to display splash image
+		 */
+		displayLoad : function () {
+			$('#syg_video_container #hook').fadeIn(900,0);
+			$('#syg_video_container #hook').html('<img src="<?php echo $syg->getImgRoot(); ?>ui/bigLoader.gif" />');
+		},
+	
+		/**
+		 * function to hide splash image
+		 */
+		hideLoad : function () {
+			$('#syg_video_container #hook').fadeOut('slow');
+		},
+		
+		/* 
+		 * function that loads the data in the table
+		 */ 
+		loadData : function (data) {
+			data = $.parseJSON(JSON.stringify(data));
+			var html;
+			
+			$('table[id^=video_entry_table-]').remove();
+		
+			$.hideLoad();
+			$.each(data, function(key, val) {
+				html = '<table class="video_entry_table-<?php echo $_GET['id'];?>">';
+				html = html + '<tr>';
+				
+				html = html + '<td class="syg_video_page_thumb-<?php echo $_GET['id'];?>">';
+				html = html + '<a class="sygVideo" href="<?php echo $syg->getPluginRoot(); ?>views/player.php?id=<?php echo $_GET['id'];?>&video=' + val.video_id + '">';
+				<?php if ($syg_description_show) { ?>
+					html = html + '<img src="' + val.video_thumbshot + '" class="thumbnail-image-<?php echo $_GET['id'];?>" alt="' + val.video_description + '" title="' + val.video_description + '"/>';
+				<?php } else { ?>
+					html = html + '<img src="<?php echo $videoThumbnails[1]['url']; ?>" class="thumbnail-image-<?php echo $_GET['id'];?>" alt="play" title="play"/>';
+				<?php }?>
+				
+				<?php 
+				$thumbImage = $option['syg_thumbnail_image'];
+				$overlayButtonSrc = (!empty($thumbImage)) ? $syg->getImgRoot() . '/button/play-the-video_' . $thumbImage .'.png' : $syg->getImgRoot() . '/button/play-the-video_1.png';
+				?>
+				
+				html = html + '<img class="play-icon-<?php echo $_GET['id'];?>" src="<?php echo $overlayButtonSrc; ?>" alt="play">';
+				
+				<?php if ($syg_description_showduration) { ?>
+					html = html + '<span class="video_duration-<?php echo $_GET['id'];?>">' + val.video_duration + '</span>';
+				<?php } ?>
+				html = html + '</a>';
+				html = html + '</td>';
+				
+				html = html + '<td class="syg_video_page_description">';
+				html = html + '<h4 class="video_title"><a href="' + val.video_watch_page_url + '" target="_blank">' + val.video_title + '</a></h4>';
+				<?php if ($syg_description_show) { ?>
+					html = html + '<p>' + val.video_description + '</p>';
+				<?php }?>
+					
+				<?php if ($syg_description_showcategories) { ?>
+					html = html + '<span class="video_categories"><i>Category:</i>&nbsp;&nbsp; ' + val.video_category + '</span>';
+				<?php } ?>
+				<?php if ($syg_description_showtags) { ?>
+					html = html + '<span class="video_tags"><i>Tags:</i>&nbsp;&nbsp;';
+					tags = val.video_tags;
+					for(var i in tags) {
+    					html = html + tags[i] + ' | '; 
+					}
+					html = html + '</span>';
+				<?php } ?>
+				<?php if ($syg_description_showratings) { ?>
+						rating = val.video_ratings;
+						if (rating) {
+							html = html + '<span class="video_ratings">';
+							html = html + '<i>Average:</i>&nbsp;&nbsp;' + rating['average'];
+							html = html + '&nbsp;&nbsp;';
+							html = html + '<i>Raters:</i>&nbsp;&nbsp;' + rating['numraters'];
+							html = html + '</span>';
+						} else {
+							html = html + '<span class="video_ratings">';
+							html = html + '<i>Rating not available</i>';
+							html = html + '</span>';
+						}
+				<?php } ?>
+				html = html + '</td>';
+				html = html + '</tr>';
+				html = html + '</table>';
+				
+				$('#syg_video_container #hook').after(html);
+			});
+		},
+		  
+		/**
+		 * function that add pagination event per table
+		 */
+		addPaginationClickEvent : function () {
+			// add galleries pagination click event
+			$('#syg-page-pagination li').click(function(){
+				$.displayLoad();
+				// css styles
+				$('#syg-page-pagination li')
+					.attr({'class' : 'other_page'});
+	
+				$(this)
+					.attr({'class' : 'current_page'});
+	
+				// loading data
+				var pageNum = this.id;
+				$.getJSON('<?php echo $syg->getJsonQueryIfUrl(); ?>?query=videos&page_number=' + pageNum + '&id=<?php echo $_GET['id']?>', function (data) {$.loadData(data);});
+			});
+		},
+		
+		getQParam : function (name) {
+			name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+			var regexS = "[\\?&]" + name + "=([^&#]*)";
+			var regex = new RegExp(regexS);
+			var results = regex.exec(window.location.search);
+			if(results == null)
+				return "";
+			else
+				return decodeURIComponent(results[1].replace(/\+/g, " "));
+			}
+		});
+})(jQuery);
+
+/************************************************
+ # jQuery on ready function:					#
+ # load fancybox and visual effects				# 
+ ************************************************/ 
 
 jQuery(document).ready(function($){
-        $(".sygVideo").fancybox({
-             'width' : <?php echo $width; ?>,  
-             'height' : <?php echo $height; ?>,
-             'autoScale' : true,  
-             'transitionIn' : 'none',  
-             'transitionOut' : 'none',  
-             'type' : 'iframe',  
-             'hideOnOverlayClick' : false,
-	     	 'autoDimensions' : false,
-	     	 'padding' : 0
-         });  
-    });  
+	// add fancybox to each sygVideo css class
+	$(".sygVideo").fancybox({
+		'width' : <?php echo $width; ?>,  
+		'height' : <?php echo $height; ?>,
+		'autoScale' : true,  
+		'transitionIn' : 'none',  
+		'transitionOut' : 'none',  
+		'type' : 'iframe',  
+		'hideOnOverlayClick' : false,
+		'autoDimensions' : false,
+		'padding' : 0
+	});
+	
+	// load if page contains a list
+	if ($('#syg_video_container').length){
+	
+		// loading images
+		$.displayLoad();
+		
+		// get the data
+		$.getJSON('<?php echo $syg->getJsonQueryIfUrl(); ?>?query=videos&page_number=1&id=<?php echo $_GET['id']; ?>', function (data) {$.loadData(data);});
+		
+		// add pagination events
+		$.addPaginationClickEvent();
+	}
+});  
--- views/page.php
+++ views/page.php
@@ -20,71 +20,20 @@ $overlayButtonSrc = (!empty($thumbImage)) ? $this->data['imgPath'] . '/button/pl
 
 <div id="syg_video_page-<?php echo $gallery->getId();?>">
 	<div class="syg_video_page_container-<?php echo $gallery->getId();?>" id="<?php echo $gallery->getId();?>">
-		<?php 
-			foreach ($feed as $element) {
-				$videoThumbnails = $element->getVideoThumbnails();	
-		?> 
-		<table class="video_entry_table-<?php echo $gallery->getId();?>">
-			<tr>
-				<td class="syg_video_page_thumb-<?php echo $gallery->getId();?>">
-					<a class="sygVideo" href="<?php echo $this->data['pluginUrl']; ?>views/player.php?id=<?php echo $gallery->getId();?>&video=<?php echo $element->getVideoId(); ?>">
-					
-					<!-- append video thumbnail -->
-					<?php if ($gallery->getDescShow()) { ?>
-						<img src="<?php echo $videoThumbnails[1]['url']; ?>" class="thumbnail-image-<?php echo $gallery->getId();?>" alt="<?php echo $element->getVideoDescription(); ?>" title="<?php echo $element->getVideoDescription(); ?>"/>
-					<?php } else { ?>
-						<img src="<?php echo $videoThumbnails[1]['url']; ?>" class="thumbnail-image-<?php echo $gallery->getId();?>" alt="play" title="play"/>
-					<?php }?>
-	
-					<!-- show overlay button -->			
-					<img class="play-icon-<?php echo $gallery->getId();?>" src="<?php echo $overlayButtonSrc; ?>" alt="play">
-						
-					<!-- show video duration -->
-					<?php if ($gallery->getDescShowDuration()) { ?>
-						<span class="video_duration-<?php echo $gallery->getId();?>"><?php echo SygUtil::formatDuration($element->getVideoDuration()); ?></span>
-					<?php } ?>
-					</a>
-				</td>
-				<td class="syg_video_page_description">
-					<h4 class="video_title"><a href="<?php echo $element->getVideoWatchPageUrl(); ?>" target="_blank"><?php echo $element->getVideoTitle(); ?></a></h4>
-					<?php if ($gallery->getDescShow()) { ?>
-						<p><?php echo $element->getVideoDescription(); ?></p>
-					<?php }?>
-					
-					<?php if ($gallery->getDescShowCategories()) { ?>
-						<span class="video_categories"><i>Category:</i>&nbsp;&nbsp; <?php echo $element->getVideoCategory(); ?></span>
-					<?php } ?>
-
-					<?php if ($gallery->getDescShowTags()) { ?>
-						<span class="video_tags"><i>Tags:</i>&nbsp;&nbsp;
-						<?php
-							foreach ($element->getVideoTags() as $key => $value) {
-								$html .= $value." | ";
-							}
-						?>
-						</span>
-					<?php } ?>
-
-					<?php if ($gallery->getDescShowRatings()) { ?>
-						<?php if ($element->getVideoRatingInfo()) { 
-								$rating = $element->getVideoRatingInfo();?>
-							<span class="video_ratings">
-							<i>Average:</i>&nbsp;&nbsp;<?php echo $rating['average'];?>
-							&nbsp;&nbsp;
-							<i>Raters:</i>&nbsp;&nbsp;<?php echo $rating['numRaters'];?>
-							</span>
-						<?php } else { ?>
-							<span class="video_ratings">
-							<i>Rating not available</i>
-							</span>
-						<?php } ?>
-					<?php } ?>
-				</td>
-			</tr>
-		</table>
-		<?php } ?>
+		<ul id="syg-page-pagination">
+			<?php
+			// show page links
+			for($i=1; $i<=$this->data['pages']; $i++) {
+				echo ($i == 1) ? '<li id="'.$i.'" class="current_page">'.$i.'</li>' : '<li id="'.$i.'">'.$i.'</li>';
+			}
+			?>
+		</ul>
 		
-		<ul id="syg-pagination-galleries">
+		<div id="syg_video_container">
+			<div id="hook"></div>
+		</div>
+		
+		<ul id="syg-page-pagination">
 			<?php
 			// show page links
 			for($i=1; $i<=$this->data['pages']; $i++) {
