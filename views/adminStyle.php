16523827beb4ddb1821a89093ebb2c0aa2e8b697
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-12 01:02:23 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@584415 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/adminStyle.php
+++ views/adminStyle.php
@@ -11,7 +11,16 @@
 <?php } ?>
 
 <?php if ($this->data['exception']) {?>
-	<div class="error"><p><strong><?php echo $this->data['exception_message']; ?></strong></p></div>
+	<div class="error">
+		<p>
+			<strong><?php echo $this->data['exception_message']; ?></strong>
+			<ul>
+				<?php foreach ($this->data['exception_detail'] as $problem) { ?>
+					<li><?php echo $problem['field'].' - '.$problem['msg']; ?></li>
+				<?php }?>
+			</ul>
+		</p>
+	</div>
 <?php } ?>
 
 <!-- Css Inclusion -->
