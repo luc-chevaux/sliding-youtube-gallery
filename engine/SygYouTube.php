4051847ef7b04286e7ccd581027765b921f0ba1a
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-24 13:51:42 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@589791 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- engine/SygYouTube.php
+++ engine/SygYouTube.php
@@ -41,15 +41,111 @@ class SygYouTube {
 	 
 	/**
 	 * @name getUserUploads
+	 * @category return youTube user uploads Feed
+	 * @since 1.0.1
+	 * @param SygGallery $gallery
+	 * @param $start = null
+	 * @param $per_page = null
+	 * @return $feed
+	 */
+	public function getUserUploads(SygGallery $gallery, $start = null, $per_page = null) {
+		$username = $gallery->getYtSrc();
+		$maxVideoCount = $gallery->getYtMaxVideoCount(); 
+		
+		// truncate feed
+		
+		// paginate feed 
+		
+		$this->yt->setMajorProtocolVersion(2);
+		if (($start !== null) && ($per_page !== null)) {
+			// return a video subset			
+			
+			$feed = new Zend_Gdata_YouTube_VideoFeed();
+			
+		} else {
+			// return all videos (warning)
+			$feed = $this->yt->retrieveAllEntriesForFeed($this->yt->getuserUploads($username));
+		}
+		
+		return $feed;
+	}
+	
+	/**
+	 * @name getUserList
+	 * @category return youTube user list Feed
+	 * @since 1.0.1
+	 * @param SygGallery $gallery
+	 * @param $start = null
+	 * @param $per_page = null
+	 * @return $feed
+	 */
+	public function getUserList(SygGallery $gallery, $start = null, $per_page = null) {
+		$list_of_videos = preg_split('/\r\n|\r|\n/', $gallery->getYtSrc());
+		foreach ($list_of_videos as $key => $value) {
+			$list_of_videos[$key] = str_replace('v=', '', parse_url($value, PHP_URL_QUERY));
+			$videoEntry = $this->getVideoEntry($list_of_videos[$key]);
+			$feed->addEntry($videoEntry);
+		}
+		$feed = $this->adjustFeed($feed, $gallery, $start, $per_page);
+		return $feed;
+	}
+	
+	/**
+	 * @name getUserPlaylist
+	 * @category return youTube user uploads
+	 * @since 1.0.1
+	 * @param SygGallery $gallery
+	 * @param $start = null
+	 * @param $per_page = null
+	 * @return $feed
+	 */
+	public function getUserPlaylist(SygGallery $gallery, $start = null, $per_page = null) {
+		$playlist_id = str_replace('list=PL', '', parse_url($gallery->getYtSrc(), PHP_URL_QUERY));
+		$content = json_decode(
+				file_get_contents(
+						'http://gdata.youtube.com/feeds/api/playlists/'
+						. $playlist_id
+						. '/?v=2&alt=json&feature=plcp'));
+		$feed_to_object = $content->feed->entry;
+		if (count($feed_to_object)) {
+			foreach ($feed_to_object as $item) {
+				$videoId = $item->{'media$group'}->{'yt$videoid'}->{'$t'};
+				$videoEntry = $this->sygYouTube->getVideoEntry($videoId);
+				$feed->addEntry($videoEntry);
+			}
+		}
+		$feed = $this->sygYouTube->adjustFeed($feed, $gallery, $start, $per_page);
+		return $feed;
+	}
+	
+	/**
+	 * @name adjustFeed
 	 * @category return youTube user uploads
 	 * @since 1.0.1
 	 * @param $username
 	 * @return $videoFeed
 	 */
-	public function getUserUploads($username) {
-		$this->yt->setMajorProtocolVersion(2);
-		$videoFeed = $this->yt->getuserUploads($username);
-		return $videoFeed;
+	private function adjustFeed(Zend_Gdata_YouTube_VideoFeed $feed, SygGallery $gallery, $start = null, $per_page = null) {
+		// truncate feed
+		$feed_count = $feed->count();
+		if ($gallery->getYtMaxVideoCount() < $feed_count) {
+			$feed_count--;
+			for ($i = $gallery->getYtMaxVideoCount(); $i <= $feed_count; $i++) {
+				$feed->offsetUnset($i);
+			}
+		}
+	
+		// feed pagination
+		if (($start !== null) && ($per_page !== null) && ($feed->count() > $per_page)) {
+			$feed_page = new Zend_Gdata_YouTube_VideoFeed();
+			$end = intval($start + $per_page) - 1;
+			for ($i = $start; $i <= $end; $i++) {
+				if ($feed->offsetGet($i)) $feed_page->addEntry($feed->offsetGet($i));
+			}
+			$feed = $feed_page;
+		}
+	
+		return $feed;
 	}
 	
 	/**
@@ -65,6 +161,13 @@ class SygYouTube {
 		return $video;
 	}
 	
+	/**
+	 * @name countGalleryEntry
+	 * @category return displayed video count
+	 * @since 1.3.0
+	 * @param $username
+	 * @return $videoFeed
+	 */
 	public function countGalleryEntry($src = null, $gallery_type = null, $max_video_count = null) {
 		$count = 0;
 		if ($src && $gallery_type && $max_video_count) {
