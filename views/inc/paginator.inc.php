0e0aac4819a1079c465bc5cba39455dae9288b78
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-24 22:28:58 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@589992 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/inc/paginator.inc.php
+++ views/inc/paginator.inc.php
@@ -1,12 +1,10 @@
-<?php if (($options['syg_option_paginationarea'] == 'top') || ($options['syg_option_paginationarea'] == 'bottom') || ($options['syg_option_paginationarea'] == 'both')) { ?>
-	<div id="paginator-<?php echo $paginator_area.'-'.$gallery->getId(); ?>">			
-		<ul id="pagination-<?php echo $paginator_area.'-'.$gallery->getId(); ?>">
-			<?php
-			// show page links
-			for($i=1; $i<=$this->data['pages']; $i++) {
-				echo ($i == 1) ? '<li id="'.$paginator_area.'-'.$i.'" class="current_page">'.$i.'</li>' : '<li id="'.$paginator_area.'-'.$i.'">'.$i.'</li>';
-			}
-			?>
-		</ul>
-	</div>
-<?php } ?>
+<div id="paginator-<?php echo $paginator_area.'-'.$gallery->getId(); ?>">			
+	<ul id="pagination-<?php echo $paginator_area.'-'.$gallery->getId(); ?>">
+		<?php
+		// show page links
+		for($i=1; $i<=$this->data['pages']; $i++) {
+			echo ($i == 1) ? '<li id="'.$paginator_area.'-'.$i.'" class="current_page">'.$i.'</li>' : '<li id="'.$paginator_area.'-'.$i.'">'.$i.'</li>';
+		}
+		?>
+	</ul>
+</div>
