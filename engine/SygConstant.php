79a6d1f59462830d6de91c39addb6cd36d69b229
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-25 10:26:56 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@590133 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- engine/SygConstant.php
+++ engine/SygConstant.php
@@ -5,7 +5,7 @@
  *
  * @author: Luca Martini @ webEng
  * @license: GNU GPLv3 - http://www.gnu.org/copyleft/gpl.html
- * @version: 1.2.5
+ * @version: 1.3.0
  */
 
 class SygConstant {
@@ -26,6 +26,7 @@ class SygConstant {
 	 */
 	const SYG_PLUGIN_COMPONENT_GALLERY = 'gallery';
 	const SYG_PLUGIN_COMPONENT_PAGE = 'page';
+	
 	/**
 	 * Plugin running action
 	 */
@@ -35,7 +36,7 @@ class SygConstant {
 	const BE_ACTION_CONTACTS = 'syg-contacts';
 
 	/**
-	 * Notification details
+	 * Notification configuration
 	 */
 	const BE_ACTION_UNINSTALL = 'uninstall';
 	const BE_ACTION_ACTIVATION = 'activation';
@@ -52,19 +53,16 @@ class SygConstant {
 	const WP_JQI_URL = '/sliding-youtube-gallery/engine/data/data.php';
 
 	/**
-	 * User interface images url
+	 * UI images url
 	 */
-
 	const BE_ICON_VIDEO_GALLERY = '../wp-content/plugins/sliding-youtube-gallery/img/ui/admin/custom_gallery.png';
 
 	/**
 	 * Sql query
 	 */
 	const SQL_GET_ALL_GALLERIES = 'SELECT id, syg_youtube_src, syg_youtube_videoformat, syg_youtube_maxvideocount, syg_youtube_disablerel, syg_description_show, syg_description_showduration, syg_description_showtags, syg_description_showratings, syg_description_showcategories, syg_style_id, syg_gallery_type, syg_gallery_name, syg_gallery_details FROM %s LIMIT %d, %d';
-	
-	// transitory variable
+	// transitory query
 	const SQL_GET_ALL_GALLERIES_1_2_X = 'SELECT id, syg_youtube_username, syg_youtube_videoformat, syg_youtube_maxvideocount, syg_thumbnail_height, syg_thumbnail_width, syg_thumbnail_bordersize, syg_thumbnail_bordercolor, syg_thumbnail_borderradius, syg_thumbnail_distance, syg_thumbnail_overlaysize, syg_thumbnail_image, syg_thumbnail_buttonopacity, syg_description_width, syg_description_fontsize, syg_description_fontcolor, syg_description_show, syg_description_showduration, syg_description_showtags, syg_description_showratings, syg_description_showcategories, syg_box_width, syg_box_background, syg_box_radius, syg_box_padding FROM %s LIMIT %d, %d';
-	
 	const SQL_GET_ALL_STYLES = 'SELECT id, syg_style_name, syg_style_details, syg_thumbnail_height, syg_thumbnail_width, syg_thumbnail_bordersize, syg_thumbnail_bordercolor, syg_thumbnail_borderradius, syg_thumbnail_distance, syg_thumbnail_overlaysize, syg_thumbnail_image, syg_thumbnail_buttonopacity, syg_description_width, syg_description_fontsize, syg_description_fontcolor, syg_box_width, syg_box_background, syg_box_radius, syg_box_padding FROM %s LIMIT %d, %d';
 	const SQL_GET_GALLERY_BY_ID = 'SELECT id, syg_youtube_src, syg_youtube_videoformat, syg_youtube_maxvideocount, syg_youtube_disablerel, syg_description_show, syg_description_showduration, syg_description_showtags, syg_description_showratings, syg_description_showcategories, syg_style_id, syg_gallery_type, syg_gallery_name, syg_gallery_details FROM %s WHERE id=%d';
 	const SQL_GET_STYLE_BY_ID = 'SELECT id, syg_style_name, syg_style_details, syg_thumbnail_height, syg_thumbnail_width, syg_thumbnail_bordersize, syg_thumbnail_bordercolor, syg_thumbnail_borderradius, syg_thumbnail_distance, syg_thumbnail_overlaysize, syg_thumbnail_image, syg_thumbnail_buttonopacity, syg_description_width, syg_description_fontsize, syg_description_fontcolor, syg_box_width, syg_box_background, syg_box_radius, syg_box_padding FROM %s WHERE id=%d';
@@ -147,6 +145,7 @@ class SygConstant {
 	const SQL_CHECK_AUTO_INCREMENT = 'SELECT AUTO_INCREMENT FROM information_schema.tables WHERE table_schema = %s AND table_name = %s';
 	const SQL_REMOVE_TABLE = 'DROP TABLE %s';
 	const SQL_SET_AUTOINCREMENT = 'ALTER TABLE %s AUTO_INCREMENT = %d';
+	
 	/**
 	 * Default values for gallery
 	 */
@@ -164,13 +163,13 @@ class SygConstant {
 	/**
 	 * Default styles for paginator
 	 */
-	const SYG_OPTION_DEFAULT_PAGINATOR_BORDERRADIUS = '0';
-	const SYG_OPTION_DEFAULT_PAGINATOR_BORDERSIZE = '1';
-	const SYG_OPTION_DEFAULT_PAGINATOR_BORDERCOLOR = '#333333';
-	const SYG_OPTION_DEFAULT_PAGINATOR_BGCOLOR = '#ffffff';
+	const SYG_OPTION_DEFAULT_PAGINATOR_BORDERRADIUS = '5';
+	const SYG_OPTION_DEFAULT_PAGINATOR_BORDERSIZE = '2';
+	const SYG_OPTION_DEFAULT_PAGINATOR_BORDERCOLOR = '#CCCCCC';
+	const SYG_OPTION_DEFAULT_PAGINATOR_BGCOLOR = '#3B393B';
 	const SYG_OPTION_DEFAULT_PAGINATOR_SHADOWCOLOR = '#000000';
-	const SYG_OPTION_DEFAULT_PAGINATOR_SHADOWSIZE = '0';
-	const SYG_OPTION_DEFAULT_PAGINATOR_FONTCOLOR = '#000000';
+	const SYG_OPTION_DEFAULT_PAGINATOR_SHADOWSIZE = '5';
+	const SYG_OPTION_DEFAULT_PAGINATOR_FONTCOLOR = '#FFFFFF';
 	const SYG_OPTION_DEFAULT_PAGINATOR_FONTSIZE = '11';
 	
 	/**
@@ -184,18 +183,13 @@ class SygConstant {
 	/**
 	 * GUI constants
 	 */
-
 	// user message
 	const BE_SUPPORT_PAGE = '<a href="http://blog.webeng.it/how-to/cms/wordpress/sliding-youtube-gallery-wordpress-plugin/">Support page</a>';
 	const BE_DONATION_CODE = '<a href="http://blog.webeng.it/how-to/cms/wordpress/sliding-youtube-gallery-wordpress-plugin/">Donation</a>';
 	const BE_NO_GALLERY_FOUND = 'No gallery found in database';
 	const BE_NO_STYLES_FOUND = 'No styles found in database';
-
-	// @todo da rivedere
 	const BE_WELCOME_MESSAGE = 'Sliding YouTube Gallery is a nice plugin, that gives you a fast way, to add video galleries in your blog directly from a youtube user\'s channel!';
-	const BE_MANAGE_GALLERY_MESSAGE = 'Height, width, border radius, border size, distance, padding and font size are treated as generic integer. You don\'t need to add px, em or other css suffix.<br/> Button opacity is a float between 0 and 1 (e.g. 0.5).';
-	const BE_CONTACT_MESSAGE = 'Il lorem ipsum è un insieme di parole utilizzato da grafici, designer, programmatori e tipografi come testo riempitivo in bozzetti e prove grafiche[1]. È un testo privo di senso, composto da parole in lingua latina (spesso storpiate), riprese pseudocasualmente da uno scritto di Cicerone del 45 a.C.';
-	const BE_DONATE_MESSAGE = 'Sliding YouTube Gallery is a nice plugin, that gives you a fast way, to add video galleries in your blog directly from a youtube user\'s channel!';
+	const BE_MANAGE_STYLE_MESSAGE = 'Height, width, border radius, border size, distance, padding and font size are treated as generic integer. You don\'t need to add px, em or other css suffix.<br/> Button opacity is a float between 0 and 1 (e.g. 0.5).';
 
 	// general menu
 	const BE_MENU_HOME = 'Home';
@@ -212,7 +206,6 @@ class SygConstant {
 	/**
 	 * Exceptions
 	 */
-
 	const MSG_EX_STYLE_NOT_VALID = 'An error was found while updating your style.';
 	const COD_EX_STYLE_NOT_VALID = '1001';
 	const MSG_EX_GALLERY_NOT_VALID = 'An error was found while updating your gallery.';
@@ -220,6 +213,9 @@ class SygConstant {
 	const MSG_EX_SETTING_NOT_VALID = 'An error was found while updating plugin settings.';
 	const COD_EX_SETTING_NOT_VALID = '1003';
 
+	/**
+	 * Validation message
+	 */
 	const BE_VALIDATE_USER_NOT_FOUND = 'There was an error in your request. YouTube user does not exist.';
 	const BE_VALIDATE_NOT_A_INTEGER = '%s is not an integer value';
 	const BE_VALIDATE_NOT_LESS_VALUE = 'value is not less than %d';
