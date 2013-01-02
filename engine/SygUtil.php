0864588a48920c740c26fa3e4a245552165027ee
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-02 02:15:20 +0000

    gd library
    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@646849 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- engine/SygUtil.php
+++ engine/SygUtil.php
@@ -186,7 +186,7 @@ class SygUtil {
 	 * @since 1.4.0
 	 * @param $folder
 	 */
-	function folder2Md5($folder) {
+	public static function folder2Md5($folder) {
 		$dircontent = scandir($folder);
 		$ret='';
 		foreach($dircontent as $filename) {
@@ -204,11 +204,87 @@ class SygUtil {
 	 * @since 1.4.0
 	 * @param $folder
 	 */
-	function cleanJsCache($folder) {
+	public static function cleanJsCache($folder) {
 		$olddate=time()-3600;
 		$dircontent = scandir($folder);
 		foreach($dircontent as $filename) {
 			if (strlen($filename)==32 && filemtime($folder.$filename) && filemtime($folder.$filename)<$olddate) unlink($folder.$filename);
+		}
+	}
+	
+	/**
+	 * @name writeResizedImage
+	 * @category resize and crop and image 
+	 * @since 1.4.0
+	 * @param $image
+	 * @param $filename
+	 * @param $crop
+	 * @param $size
+	 */
+	public static function writeResizedImage($image, $filename, $crop = null, $size = null) {
+		$image = ImageCreateFromString(file_get_contents($image));
+
+		if (is_resource($image) === true) {
+			$x = 0;
+			$y = 0;
+			$width = imagesx($image);
+			$height = imagesy($image);
+	
+			/* CROP (Aspect Ratio) Section */
+			if (is_null($crop) === true) {
+				$crop = array($width, $height);
+			} else {
+				$crop = array_filter(explode(':', $crop));
+	
+				if (empty($crop) === true) {
+					$crop = array($width, $height);
+				} else {
+					if ((empty($crop[0]) === true) || (is_numeric($crop[0]) === false)) {
+						$crop[0] = $crop[1];
+					} else if ((empty($crop[1]) === true) || (is_numeric($crop[1]) === false)) {
+						$crop[1] = $crop[0];
+					}
+				}
+	
+				$ratio = array(0 => $width / $height, 1 => $crop[0] / $crop[1]);
+	
+				if ($ratio[0] > $ratio[1]) {
+					$width = $height * $ratio[1];
+					$x = (imagesx($image) - $width) / 2;
+				} else if ($ratio[0] < $ratio[1]) {
+					$height = $width / $ratio[1];
+					$y = (imagesy($image) - $height) / 2;
+				}
+			}
+	
+			/* Resize Section */
+			if (is_null($size) === true) {
+				$size = array($width, $height);
+			} else {
+				$size = array_filter(explode('x', $size));
+	
+				if (empty($size) === true) {
+					$size = array(imagesx($image), imagesy($image));
+				} else {
+					if ((empty($size[0]) === true) || (is_numeric($size[0]) === false)) {
+						$size[0] = round($size[1] * $width / $height);
+					} else if ((empty($size[1]) === true) || (is_numeric($size[1]) === false)) {
+						$size[1] = round($size[0] * $height / $width);
+					}
+				}
+			}
+	
+			$result = ImageCreateTrueColor($size[0], $size[1]);
+	
+			if (is_resource($result) === true) {
+				ImageSaveAlpha($result, true);
+				ImageAlphaBlending($result, true);
+				ImageFill($result, 0, 0, ImageColorAllocate($result, 255, 255, 255));
+				ImageCopyResampled($result, $image, 0, 0, $x, $y, $size[0], $size[1], $width, $height);
+	
+				ImageInterlace($result, true);
+				ImageJPEG($result, $filename, 90);
+			}
 		}
 	}
 }
