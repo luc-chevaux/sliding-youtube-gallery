e8b682c33ee1c1c8876ca85f968c46578cb7b030
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-10 21:23:42 +0000

    support update
    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@651030 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/adminSupport.php
+++ views/adminSupport.php
@@ -25,10 +25,35 @@
 	<div id="syg-plugin-area">
 		<table>
 			<tr>
-				<td><img src="http://s-plugins.wordpress.org/sliding-youtube-gallery/assets/banner-772x250.png" height="150" style="border: 1px #333 solid;"/></td>
-				<td>
-					<div style="background-color: #cccccc; border: 1px #333 solid; padding: 10px; height: 130px;">
-						<b>Quick resources</b>
+				<td style="vertical-align: top;">
+					<h3>Project philosophy</h3>
+					<p>SYG is a nice plugin that gives you a fast way, to add multiple and fully customizable ajax video galleries from different sources in your blog!</p>
+					<p>You can add video from different sources such as user's channel, youtube playlist or by adding video url manually. You may choose to display the videos in a fully customizable horizontal sliding gallery or if you prefer, you can get displayed as a paginated table-based component. Users can get the video played as a nice fancybox player.</p>
+					<p>If you have problems during the update, please read UPGRADE NOTICE @ Sliding YouTube Gallery support forum.</p>
+					<h3>What you can do?</h3>
+					<ul class="">
+						<li>Buy me a beer. Please consider to donate with paypal!</li>
+						<li>Submit a problem to our bug database</li>
+						<li>Submit your idea on our blog</li>
+						<li>Write an article on this plugin</li>
+						<li>Submit a support request</li>
+					</ul>
+					
+					<h3>Features request</h3>
+					
+					<h3>Join the plugin as developer</h3>
+					<p>If you're a php developer experienced in WordPress and you like the plugin idea, you might want to contribute to this project, you have just only to ask me.</p>
+					
+					<h3>Third party library</h3>
+					<a href="https://github.com/Emerson/Sanity-Wordpress-Plugin-Framework">Sanity framework</a> | <a href="http://www.eyecon.ro/colorpicker/">Eyecon colorpicker</a> | <a href="http://www.professorcloud.com/mainsite/carousel.htm">3d Cloud Carousel</a> | <a href="http://fancybox.net/">Fancybox</a>
+					<h3>Project roadmap</h3>
+					<hr/>
+					<p class="webengText">Thank you for downloading! Enjoy WP and OpenSource!</p>
+					<p class="webengText">Luca</p>
+				</td>
+				<td style="vertical-align: top;">
+					<div style="background-color: #cccccc; border: 1px #333 solid; padding: 10px; margin-bottom: 15px; border-radius: 10px;">
+						<h4>Quick resources</h4>
 						<ul>
 							<li><a href="">Project homepage</a></li>
 							<li><a href="http://wordpress.org/extend/plugins/sliding-youtube-gallery/">Plugin homepage</a></li>
@@ -36,74 +61,41 @@
 							<li><a href="">Support forum</a></li>
 						</ul>
 					</div>
-				</td>
-				<td>
-				<div style="background-color: #cccccc; border: 1px #333 solid; padding: 10px; height: 130px;">
-					<b>Buy me a beer!</b>
-					<p>Buy me a beer. Please consider to donate with paypal!</p>
-					<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="FRXA5225YQMUU">
-					<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
-					<img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
-					</form>
-				</div>
-				</td>
-				<td>
-					<div style="background-color: #cccccc; border: 1px #333 solid; padding: 10px; height: 130px;">
-						<b>Social activities</b>
+					<div style="background-color: #cccccc; border: 1px #333 solid; padding: 10px; margin-bottom: 15px; border-radius: 10px;">
+						<h4>Buy me a beer!</h4>
+						<p>Buy me a beer. Please consider to donate with paypal!</p>
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="hosted_button_id" value="FRXA5225YQMUU">
+						<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
+						<img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
+						</form>
+					</div>
+					<div style="background-color: #cccccc; border: 1px #333 solid; padding: 10px; margin-bottom: 15px; border-radius: 10px;">
+						<h4>Social activities</h4>
 						<p>Please recomend this plugin over your social network.</p>
-					<div class="fb-like" data-href="http://wordpress.org/extend/plugins/sliding-youtube-gallery/" data-send="true" data-layout="button_count" data-width="450" data-show-faces="true" data-font="verdana" data-colorscheme="dark"></div>
+						<div class="fb-like" data-href="http://wordpress.org/extend/plugins/sliding-youtube-gallery/" data-send="true" data-layout="button_count" data-width="450" data-show-faces="true" data-font="verdana" data-colorscheme="dark"></div>
 					
-					<br/>	
-					<!-- Inserisci questo tag nel punto in cui vuoi che sia visualizzato l'elemento pulsante +1. -->
-					<div class="g-plusone" data-href="http://wordpress.org/extend/plugins/sliding-youtube-gallery/"></div>
-					
-					<!-- Inserisci questo tag dopo l'ultimo tag di pulsante +1. -->
-					<script type="text/javascript">
-					  window.___gcfg = {lang: 'en-GB'};
-					
-					  (function() {
-					    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-					    po.src = 'https://apis.google.com/js/plusone.js';
-					    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-					  })();
-					</script>
-									<br/>	
-				
-					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://wordpress.org/extend/plugins/sliding-youtube-gallery/" data-text="Sliding YouTube Gallery">Tweet</a>
-					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+						<br/>	
+						<!-- Inserisci questo tag nel punto in cui vuoi che sia visualizzato l'elemento pulsante +1. -->
+						<div class="g-plusone" data-href="http://wordpress.org/extend/plugins/sliding-youtube-gallery/"></div>
+						
+						<!-- Inserisci questo tag dopo l'ultimo tag di pulsante +1. -->
+						<script type="text/javascript">
+						  window.___gcfg = {lang: 'en-GB'};
+						
+						  (function() {
+						    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+						    po.src = 'https://apis.google.com/js/plusone.js';
+						    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+						  })();
+						</script>
+						<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://wordpress.org/extend/plugins/sliding-youtube-gallery/" data-text="Sliding YouTube Gallery">Tweet</a>
+						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 					</div>
 				</td>
 			</tr>
 		</table>
-		<h3>Project philosophy</h3>
-		<p>Sliding YouTube Gallery is a plugin that allows you to generate custom web components basing on your youtube videos.</p>
-		<h3>What you can do?</h3>
-		<ul class="">
-			<li>Buy me a beer. Please consider to donate with paypal!</li>
-			<li>Submit a problem to our bug database</li>
-			<li>Submit your idea on our blog</li>
-			<li>Write an article on this plugin</li>
-			<li>Submit a support request</li>
-		</ul>
-		
-		<h3>Features request</h3>
-		
-		<h3>Join the plugin as developer</h3>
-		<p>If you're a WP or PHP developer</strong> and you like the plugin idea, if you have some time you can contribute to this plugin! Sometimes two developer is better than one, right?</p>
-		
-		<h3>Third party library</h3>
-		<a href="https://github.com/Emerson/Sanity-Wordpress-Plugin-Framework">Sanity framework</a>
-		<a href="http://www.eyecon.ro/colorpicker/">Eyecon colorpicker</a>
-		<a href="http://www.professorcloud.com/mainsite/carousel.htm">3d Cloud Carousel</a>
-				<a href="http://fancybox.net/">Fancybox</a>
-		
-		<h3>Project roadmap</h3>
-		<hr/>
-		<p class="webengText">Thank you for downloading! Enjoy WP and OpenSource!</p>
-		<p class="webengText">Luca</p>
-		
 		<?php require_once 'inc/contextMenu.inc.php'; ?>
 	</div>
 </div>
