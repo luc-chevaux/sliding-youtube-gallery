5eeda3332873458ddcd4c37d0201a0edac8061c2
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-27 02:07:17 +0000

    sanity update
    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@659462 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- engine/lib/Sanity/sanity.php
+++ engine/lib/Sanity/sanity.php
@@ -143,6 +143,10 @@ class SanityPluginFramework {
         	$url = WP_PLUGIN_URL.'/sliding-youtube-gallery/js/core/ui/ready/syg.client.min.js.php?id='.$id.'&cache=on'.'&ui='.SygConstant::SYG_PLUGIN_COMPONENT_PAGE;
         	wp_register_script('syg-client-'.$id.'-'.SygConstant::SYG_PLUGIN_COMPONENT_PAGE, $url, array(), SygConstant::SYG_VERSION, true);
         	wp_enqueue_script('syg-client-'.$id.'-'.SygConstant::SYG_PLUGIN_COMPONENT_PAGE);
+        	// js to include
+        	$url = WP_PLUGIN_URL.'/sliding-youtube-gallery/js/core/ui/loading/page.min.js.php?id='.$gallery->getId();
+        	wp_register_script('syg-action-'.$gallery->getId(), $url, array(), SygConstant::SYG_VERSION, true);
+        	wp_enqueue_script('syg-action-'.$gallery->getId());
         } elseif (SygConstant::SYG_PLUGIN_COMPONENT_CAROUSEL == $component) {
         	// js to include
         	$url = WP_PLUGIN_URL.'/sliding-youtube-gallery/js/core/ui/ready/syg.client.min.js.php?id='.$id.'&cache=on'.'&ui='.SygConstant::SYG_PLUGIN_COMPONENT_CAROUSEL;
