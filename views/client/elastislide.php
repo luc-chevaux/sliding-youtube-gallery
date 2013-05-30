17729d9049cd3d5e7a68e01d69e17d68eccc3a17
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-30 22:42:18 +0000

    elastislide
    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@720587 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/client/elastislide.php
+++ views/client/elastislide.php
@@ -1,6 +1,3 @@
-<!-- Php Inclusion -->
-
-<!-- Extra Php Code -->
 <?php
 // gallery data retreival
 $feed = $this->data['feed'];
@@ -12,54 +9,43 @@ $options = $this->data['options'];
 $thumbImage = $gallery->getSygStyle()->getThumbImage();
 $overlayButtonSrc = (!empty($thumbImage)) ? $this->data['imgPath'] . '/button/play-the-video_' . $gallery->getSygStyle()->getThumbImage() .'.png' : $this->data['imgPath'] . '/button/play-the-video_1.png'; 
 ?>
-<!-- User Message -->
-
-<!-- Css Inclusion -->
-
-<!-- Javascript Inclusion -->
-
-<!-- Gallery -->
-
-				<!-- Elastislide Carousel -->
-				<ul id="syg-elastislide-<?php echo $gallery->getId();?>" class="elastislide-list">
-					<?php 
-					foreach ($feed as $element) {				
-						// modify the img path to match local files
-						if ($mode == SygConstant::SYG_PLUGIN_FE_CACHING_MODE) {
-							$videoThumbnails[$options['syg_option_which_thumb']]['url'] = WP_PLUGIN_URL . 
-														SygConstant::WP_PLUGIN_PATH .
-														SygConstant::WP_CACHE_THUMB_REL_DIR .
-														$gallery->getId() . 
-														DIRECTORY_SEPARATOR . 
-														$element->getVideoId() . '.jpg';
-						} else {
-							$videoThumbnails = $element->getVideoThumbnails();
-						}
-					?> 
-					<li>
-						<a class="sygVideo-<?php echo $gallery->getId();?>" href="http://www.youtube.com/watch?v=<?php echo $element->getVideoId(); ?>" title="<?php echo $element->getVideoTitle(); ?>">
-							<!-- append video thumbnail -->
-							<?php if ($gallery->getDescShow()) { ?>
-								<img src="<?php echo $videoThumbnails[$options['syg_option_which_thumb']]['url']; ?>" class="thumbnail-image-<?php echo $gallery->getId();?>" alt="<?php echo $element->getVideoDescription(); ?>" title="<?php echo $element->getVideoDescription(); ?>"/>
-							<?php } else { ?>
-								<img src="<?php echo $videoThumbnails[$options['syg_option_which_thumb']]['url']; ?>" class="thumbnail-image-<?php echo $gallery->getId();?>" alt="play" title="play"/>
-							<?php }?>				
-					
-							<!-- show overlay button -->
-							<?php if ((!$gallery->getCacheOn()) || (!$gallery->isGalleryCached())) { ?>			
-								<img class="play-icon-<?php echo $gallery->getId();?>" src="<?php echo $overlayButtonSrc; ?>" alt="play">
-							<?php } ?>
-							
-							<!-- show video duration -->
-							<?php if ($gallery->getDescShowDuration()) { ?>
-								<span class="video_duration-<?php echo $gallery->getId();?>"><?php echo SygUtil::formatDuration($element->getVideoDuration()); ?></span>
-							<?php } ?>
-						</a>
-					</li>
-					<?php } ?>
-				</ul>
-				<!-- End Elastislide Carousel -->
-<!-- registra script -->
+<ul id="syg-elastislide-<?php echo $gallery->getId();?>" class="elastislide-list">
+	<?php 
+	foreach ($feed as $element) {				
+		// modify the img path to match local files
+		if ($mode == SygConstant::SYG_PLUGIN_FE_CACHING_MODE) {
+			$videoThumbnails[$options['syg_option_which_thumb']]['url'] = WP_PLUGIN_URL . 
+										SygConstant::WP_PLUGIN_PATH .
+										SygConstant::WP_CACHE_THUMB_REL_DIR .
+										$gallery->getId() . 
+										DIRECTORY_SEPARATOR . 
+										$element->getVideoId() . '.jpg';
+		} else {
+			$videoThumbnails = $element->getVideoThumbnails();
+		}
+	?> 
+	<li>
+		<a href="http://www.youtube.com/watch?v=<?php echo $element->getVideoId(); ?>" title="<?php echo $element->getVideoTitle(); ?>">
+			<!-- append video thumbnail -->
+			<?php if ($gallery->getDescShow()) { ?>
+				<img src="<?php echo $videoThumbnails[$options['syg_option_which_thumb']]['url']; ?>" alt="<?php echo $element->getVideoDescription(); ?>" title="<?php echo $element->getVideoDescription(); ?>"/>
+			<?php } else { ?>
+				<img src="<?php echo $videoThumbnails[$options['syg_option_which_thumb']]['url']; ?>" alt="play" title="play"/>
+			<?php }?>				
+	
+			<!-- show overlay button -->
+			<?php if ((!$gallery->getCacheOn()) || (!$gallery->isGalleryCached())) { ?>			
+				<img class="play-icon-<?php echo $gallery->getId();?>" src="<?php echo $overlayButtonSrc; ?>" alt="play">
+			<?php } ?>
+			
+			<!-- show video duration -->
+			<?php if ($gallery->getDescShowDuration()) { ?>
+				<span class="video_duration-<?php echo $gallery->getId();?>"><?php echo SygUtil::formatDuration($element->getVideoDuration()); ?></span>
+			<?php } ?>
+		</a>
+	</li>
+	<?php } ?>
+</ul>
 <?php 
 // js to include
 $url = WP_PLUGIN_URL.'/sliding-youtube-gallery/js/core/ui/ready/syg.client.min.js.php?id='.$gallery->getId().'&cache=off'.'&ui='.SygConstant::SYG_PLUGIN_COMPONENT_ELASTISLIDE;
