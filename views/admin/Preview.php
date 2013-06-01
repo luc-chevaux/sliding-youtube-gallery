b4aad8d268e1d6fe2e2602540f1b24fd60877e99
webeng <webeng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-01 22:06:23 +0000

    notice soppressed
    git-svn-id: http://plugins.svn.wordpress.org/sliding-youtube-gallery/trunk@721482 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/admin/Preview.php
+++ views/admin/Preview.php
@@ -2,6 +2,18 @@
 	error_reporting(0);
 	require_once 'inc/getWpEnvironment.inc.php';
 	require_once 'inc/getExtractedGallery.inc.php';
+	
+	// gallery loading 
+	$galleryLoadingJs = WP_PLUGIN_URL.'/sliding-youtube-gallery/js/core/ui/loading/gallery.js.php?id='.$gallery_id;
+	if (array_key_exists('params', $_GET)) {
+		$galleryLoadingJs .= '&params='.urlencode($_GET['params']);
+	}
+	
+	// gallery css
+	$galleryCss = WP_PLUGIN_URL.'/sliding-youtube-gallery/css/SygClient.css.php?id=' . $gallery_id;
+	if (array_key_exists('params', $_GET)) {
+		$galleryCss .= '&params=' . urlencode($_GET['params']);
+	}
 ?>
 <?php 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
@@ -26,7 +38,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	</style>
 	<script type="text/javascript" src="../../../../../wp-includes/js/jquery/jquery.js"></script>
 	<script type="text/javascript" src="<?php echo WP_PLUGIN_URL.'/sliding-youtube-gallery/js/core/lib/syg.lib.client.min.js.php'; ?>"></script>
-	<script type="text/javascript" src="<?php echo WP_PLUGIN_URL.'/sliding-youtube-gallery/js/core/ui/loading/gallery.js.php?id='.$gallery_id.'&params='.http_build_query($_GET, '', '|'); ?>"></script>
+	<script type="text/javascript" src="<?php echo $galleryLoadingJs; ?>"></script>
 </head>
 <body>
 	<div id="loading-level"></div>
@@ -37,7 +49,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	</div>
 	<div id="loading-footer">
 		<style type="text/css">
-			@import url('<?php echo $view['cssPath'].'SygClient.css.php?id=' . $gallery_id. '&params=' . http_build_query($_GET, '', '|'); ?>');			
+			@import url('<?php echo $galleryCss; ?>');			
 			@import url('<?php echo WP_PLUGIN_URL.'/sliding-youtube-gallery/js/3rdParty/fancybox/jquery.fancybox-1.3.4.css'; ?>');
 		</style>		
 		<script type="text/javascript">
